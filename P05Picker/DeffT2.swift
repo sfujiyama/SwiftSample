347686d7f49231488b8f3ee5f764e765b74aa0f0
Soseki Fujiyama <sin.fukase.nine@gmail.com>
2020-06-30 20:50:43 +0900

    first

---
+++ P05Picker/DeffT2.swift
@@ -0,0 +1,19 @@
+//
+//  DeffT2.swift
+//  P05Picker
+//
+//  Created by pleocene on 2020/06/30.
+//  Copyright © 2020 S.Fukase. All rights reserved.
+//
+
+import Foundation
+
+struct tempStr {
+    var msg = ""
+    func test(){
+        debugPrint("1a")
+        debugPrint("1b")
+        debugPrint("1c")
+    }
+}
+
